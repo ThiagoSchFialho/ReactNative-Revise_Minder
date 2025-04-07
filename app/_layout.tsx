ecfdf4eff9897faf57c4604cecb8021e8e81b9a3
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-04-07 20:17:14 -0300

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="screens/home/home" options={{ title: "Home" }} />
+    <Stack>
+      <Stack.Screen name="screens/home" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/screens/home/styles.ts
+++ app/screens/home/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { theme } from "../../../constants/theme";
+import { theme } from "../../constants/theme";
 
 export const styles = StyleSheet.create({
   container: {
