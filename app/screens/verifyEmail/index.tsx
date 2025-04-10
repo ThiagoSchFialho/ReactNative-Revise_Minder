1e455351ece84dceadaeac123ac6f0ae1150a5cb
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-04-10 20:54:44 -0300

    feat: Verify email page

---
+++ app/screens/verifyEmail/index.tsx
@@ -0,0 +1,85 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import { useState, useRef } from "react";
+
+export default function ForgotPassword() {
+  const [code, setCode] = useState<string[]>(["", "", "", "", "", ""]);
+  const [lastEditedIndex, setLastEditedIndex] = useState<number | null>(null);
+  const inputRefs = useRef<(TextInput | null)[]>([]);
+
+  const handleCodeChange = (text: string, index: number) => {
+    if (text.length > 1) return;
+
+    const newCode = [...code];
+    newCode[index] = text;
+    setCode(newCode);
+    setLastEditedIndex(index);
+
+    if (text && index < 5) {
+      inputRefs.current[index + 1]?.focus();
+    }
+    if (!text && index > 0) {
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
+
+  const handleFocus = (index: number) => {
+    if (lastEditedIndex !== null) {
+      const nextIndex = Math.min(lastEditedIndex + 1, 5);
+      inputRefs.current[nextIndex]?.focus();
+    } else {
+      inputRefs.current[0]?.focus();
+    }
+  };
+
+  const handleVerificationCode = () => {
+    const fullCode = code.join("");
+    alert(fullCode);
+  };
+
+  const handleResendVerificationCode = () => {
+    alert("Reenviando código");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("@/app/assets/images/logo.png")}
+      />
+
+      <Text style={styles.title}>Código de verificação enviado para:</Text>
+      <Text style={styles.text}>email@example.com</Text>
+
+      <View>
+        <View style={styles.inputContainer}>
+          {code.map((digit, index) => (
+            <TextInput
+              key={index}
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              value={digit}
+              onChangeText={(text) => handleCodeChange(text, index)}
+              ref={(ref) => (inputRefs.current[index] = ref)}
+              onFocus={() => handleFocus(index)}
+              onKeyPress={({ nativeEvent }) => {
+                if (nativeEvent.key === "Backspace" && !digit && index > 0) {
+                  inputRefs.current[index - 1]?.focus();
+                }
+              }}
+            />
+          ))}
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={handleVerificationCode}>
+          <Text style={styles.buttonText}>Verificar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.resendButton} onPress={handleResendVerificationCode}>
+          <Text style={styles.resendButtonText}>Reenviar Código</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ app/screens/verifyEmail/styles.ts
@@ -0,0 +1,75 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { theme } from "../../constants/theme";
+
+const screenWidth = Dimensions.get("window").width;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.background,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: 301,
+    height: 82,
+    marginHorizontal: "auto",
+    marginVertical: 40,
+  },
+  title: {
+    fontFamily: "Montserrat-SemiBold",
+    color: theme.text,
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  text: {
+    fontFamily: "Montserrat-SemiBold",
+    color: theme.text,
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 80,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: screenWidth - 60,
+    marginBottom: 30,
+  },
+  input: {
+    width: 40,
+    borderBottomWidth: 1,
+    borderColor: theme.text,
+    fontSize: 30,
+    fontFamily: "Montserrat-Medium",
+    color: theme.text,
+    textAlign: "center",
+    paddingBottom: 0
+  },
+  button: {
+    backgroundColor: theme.mainColor,
+    borderRadius: 10,
+    paddingVertical: 10,
+    marginTop: 80,
+    marginBottom: 30,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: theme.text,
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 24,
+  },
+  resendButton: {
+    width: screenWidth * 0.5,
+    backgroundColor: theme.secondBackground,
+    borderRadius: 5,
+    paddingVertical: 12,
+    alignSelf: "center",
+  },
+  resendButtonText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontFamily: "Montserrat-SemiBold",
+    color: theme.secondText,
+  },
+});
